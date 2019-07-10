3bc1beb7a7f43ad7bd36441141042334f053b50e
Parker Redcross <redcross.parker@gmail.com>
2019-07-09 23:56:09 -0700

    [test] virtex and format tests added
    Tests added for basic virtex manipulation and format outputting.
    Formatting tests should be made more robust.

---
+++ test/format.test.c
@@ -0,0 +1,42 @@
+#include <string.h>
+#include <stdio.h>
+
+#define FORMAT_LITERAL_EXPECT "euler"
+TEST("Format Literal") {
+  vtx* v = vtx_create(VT_LITERAL);
+  vtx_text(v, "euler");
+
+  char** fmt_string = vtx_format(v);
+  EXPECT(v->width == strlen(FORMAT_LITERAL_EXPECT));
+  EXPECT(v->height == 1);
+  EXPECT(v->baseline == 0);
+  EXPECT(strcmp(*fmt_string, FORMAT_LITERAL_EXPECT) == 0);
+
+  fmt_free(v, fmt_string);
+  vtx_destroy(v);
+  PASS();
+}
+
+#define FORMAT_SUM_EXPECT "3.14 + 2.718"
+TEST("Format Sum") {
+  vtx* v = vtx_create(VT_SUM);
+  vtx* lit_pi = vtx_create(VT_LITERAL);
+  vtx* lit_e = vtx_create(VT_LITERAL);
+
+  vtx_text(lit_pi, "3.14");
+  vtx_text(lit_e, "2.718");
+
+  vtx_insert(v, lit_pi);
+  vtx_insert(v, lit_e);
+  
+  char** fmt_string = vtx_format(v);
+  EXPECT(v->width == strlen(FORMAT_SUM_EXPECT));
+  EXPECT(v->height == 1);
+  EXPECT(v->baseline == 0);
+  EXPECT(strcmp(*fmt_string, FORMAT_SUM_EXPECT) == 0);
+
+  fmt_free(v, fmt_string);
+  vtx_destroy(v);
+  PASS();
+}
+  
---
+++ test/virtex.test.c
@@ -0,0 +1,56 @@
+#include <string.h>
+TEST("Create Virtex") {
+  vtx* v = vtx_create(VT_LITERAL);
+
+  EXPECT(v != NULL);
+  EXPECT(v->type == VT_LITERAL);
+  EXPECT(v->childCount == 0);
+  EXPECT(v->childCapacity == VTX_INIT_CHILDCAPACITY);
+
+  vtx_destroy(v);
+  PASS();
+}
+
+TEST("Literal Virtex Set Text") {
+  vtx* v = vtx_create(VT_LITERAL);
+  
+  EXPECT(vtx_text(v, "euler") == 0);
+  EXPECT(strcmp(v->childNodes[0].text, "euler") == 0);
+
+  vtx_destroy(v);
+  PASS();
+}
+
+TEST("Sum Virtex Set Text") {
+  vtx* v = vtx_create(VT_SUM);
+
+  EXPECT(vtx_text(v, "euler") == -1);
+
+  vtx_destroy(v);
+  PASS();
+}
+
+TEST("Literal Virtex Insert") {
+  vtx* v = vtx_create(VT_LITERAL);
+  vtx* i = vtx_create(VT_LITERAL);
+
+  EXPECT(vtx_insert(v, i) == -1);
+  EXPECT(v->childCount == 0);
+
+  vtx_destroy(v);
+  vtx_destroy(i);
+  PASS();
+}
+
+TEST("Sum Virtex Insert") {
+  vtx* v = vtx_create(VT_SUM);
+  vtx* i = vtx_create(VT_LITERAL);
+
+  EXPECT(vtx_insert(v, i) == 0);
+  EXPECT(v->childCount == 1);
+  EXPECT(v->childNodes[0].item->type == VT_LITERAL);
+
+  vtx_destroy(v);
+  PASS();
+}
+
